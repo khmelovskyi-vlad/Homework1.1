67ec9cfcbb1b392aee0b06c5490cc06fca421c9a
vladhmelevskiy1234765 <vladhmelevskiy1234765@gmail.com>
2019-06-02 14:40:11 +0300

    initial

---
+++ homework1.1/homework1.1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("It is game called 'Guess the number'");
+            Random rand = new Random();
+            var randomNumber = CreatRandomNumber(rand);
+            var userInput = ReadPos("your number");
+            int tryCounter = 1;
+            do
+            {
+                if (userInput > randomNumber)
+                {
+                    Console.WriteLine("Your number is bigger than computer number");
+                }
+                else
+                {
+                    Console.WriteLine("Your number is small than computer number");
+                }
+                userInput = ReadPos("your number again");
+                tryCounter++;
+            } while (userInput != randomNumber);
+            Console.WriteLine("You are right");
+            Console.WriteLine($"The number of attempts is: {tryCounter}");
+            Console.Read();
+        }
+
+        private static int CreatRandomNumber(Random rend)
+        {
+            var startPos = ReadPos("intirial range");
+            var endPos = ReadPos("end range");
+            while (true)
+            {
+                try
+                {
+                    var k = rend.Next(startPos, endPos);
+                    return k;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        static int ReadPos(string posname)
+        {
+                do
+                {
+                    try
+                    {
+                        Console.WriteLine($"Enter {posname}");
+                        var key = Console.ReadKey();
+                        if (key.Key == ConsoleKey.Escape)
+                        {
+                            throw new OperationCanceledException();
+                        }
+                        var line = Console.ReadLine();
+                        var key_line = $"{key.KeyChar}{line}";
+                        return Convert.ToInt32(key_line);
+                    }
+                    catch (FormatException ex)
+                    {
+                        Console.WriteLine($"Bed Input {ex.Message}. Enter new or press esc");
+                    }
+                } while (true);
+        }
+
+
+    }
+}
